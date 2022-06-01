3ae9c681c0bee644f4eca9e964f3ceed41057061
samuel <sg@eightda.ch>
2022-06-01 11:22:36 -0300

    feat: compoentizing layout

---
+++ src/components/layout/layout.components.tsx
@@ -0,0 +1,66 @@
+import React, { ReactNode } from 'react'
+import Image from 'next/image'
+import {
+  Container,
+  Header,
+  Left,
+  Right,
+  Logo,
+  LogoSecond,
+  ContainerAvatarHeader,
+  TextAvatar,
+  ContainerTexts,
+  Name,
+  Description,
+  Main,
+  Rectangle,
+  Dashboard,
+  Content,
+} from './layout.styles'
+import logoPrimary from '../../../public/vector-logo-primary.svg'
+import logoSecondary from '../../../public/vector-logo-secondary.svg'
+import Avatar from '@mui/material/Avatar'
+import { colors } from '../../styles/global'
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <Container>
+      <Header>
+        <Left>
+          <Logo>
+            <Image src={logoPrimary} width={29} height={29} />
+          </Logo>
+
+          <LogoSecond>
+            <Image src={logoSecondary} width={29} height={29} />
+          </LogoSecond>
+        </Left>
+        <Right>
+          <ContainerAvatarHeader>
+            <Avatar sx={{ bgcolor: colors.statusColor, width: 32, height: 32 }}>
+              <TextAvatar>LZ</TextAvatar>
+            </Avatar>
+          </ContainerAvatarHeader>
+          <ContainerTexts>
+            <Name>Luiz Zlochevsky</Name>
+            <Description>meus dados</Description>
+          </ContainerTexts>
+        </Right>
+      </Header>
+
+      <Main>
+        <Rectangle />
+
+        <Dashboard>
+          <Content>{children}</Content>
+        </Dashboard>
+      </Main>
+    </Container>
+  )
+}
+
+export default Layout
---
+++ src/components/layout/layout.styles.tsx
@@ -0,0 +1,145 @@
+import styled from '@emotion/styled'
+import { colors } from '../../styles/global'
+import { textDefault } from '../../styles/global'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Header = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: ${colors.white};
+  font-size: 1.4rem;
+  border-bottom: 1px solid ${colors.gray};
+  padding: 0 32px;
+  display: flex;
+  justify-content: space-between;
+
+  @media (max-width: 600px) {
+    padding: 0;
+    justify-content: flex-end;
+    flex-direction: row-reverse;
+    gap: 0;
+  }
+`
+
+export const Left = styled.div`
+  height: 100%;
+  width: 67px;
+  border-right: 1px solid ${colors.gray};
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 600px) {
+    border: 0px;
+    flex: 1;
+    margin-right: 50px;
+    justify-content: center;
+  }
+`
+
+export const Right = styled.div`
+  height: 100%;
+  padding-left: 32px;
+  width: 184px;
+  border-left: 1px solid ${colors.gray};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+
+  @media (max-width: 600px) {
+    border: 0px;
+    padding-left: 0px;
+    width: 50px;
+  }
+`
+
+export const Logo = styled.div`
+  position: relative;
+  margin-top: -6px;
+  z-index: 1;
+`
+export const LogoSecond = styled.div`
+  position: relative;
+  margin-left: -24px;
+  z-index: 2;
+  opacity: 0.85;
+`
+
+export const ContainerAvatarHeader = styled.div`
+  width: 32px;
+  height: '100%';
+  display: flex;
+  align-items: center;
+`
+
+export const TextAvatar = styled(textDefault)`
+  font-size: 1.4rem;
+`
+
+export const ContainerTexts = styled.div`
+  width: 110px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media (max-width: 600px) {
+    display: none;
+  }
+`
+
+export const Name = styled(textDefault)`
+  font-weight: 600;
+  font-size: 1.4rem;
+  line-height: 1.4rem;
+`
+export const Description = styled(textDefault)`
+  color: ${colors.background};
+  font-weight: 400;
+  font-size: 1.2rem;
+  line-height: 1.44rem;
+`
+
+export const Main = styled.div`
+  width: 100%;
+  height: calc(100vh - 60px);
+  display: flex;
+  flex-direction: row;
+`
+export const Rectangle = styled.div`
+  flex: 1;
+  max-width: 256px;
+  position: relative;
+  z-index: 2;
+  background-color: ${colors.white};
+  box-shadow: 4px 0px 8px 0px rgba(165, 171, 179, 0.16);
+
+  @media (max-width: 1024px) {
+    animation: in-out 3s forwards;
+  }
+
+  @keyframes in-out {
+    from {
+      max-with: 256px;
+    }
+    to {
+      max-width: 0px;
+    }
+  }
+`
+
+export const Dashboard = styled.div`
+  flex: 1;
+  background-color: ${colors.background};
+  padding-top: 56px;
+`
+
+export const Content = styled.div`
+  max-width: 58%;
+  height: 100%;
+  margin: auto;
+`
